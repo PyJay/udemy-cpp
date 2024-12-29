c250a7db47b464a71c27f1149bd33ee1bdfc69c6
Jay Shah <jrshah01@gmail.com>
2024-12-29 01:07:52 +0000

    finished the loops challenge (skipped disallowing duplicates because implemented count occurence instead)

--- section_9_controlling_program_flow/src/loops_challenge.cpp
+++ section_9_controlling_program_flow/src/loops_challenge.cpp
@@ -70,12 +70,14 @@ void main(){
     vector<int> nums{};
     do
     {
-    cout << "\nPick an option from these:" << "\n" << "P - Print numbers" << "\n"
+    cout << "\n\nPick an option from these:" << "\n" << "P - Print numbers" << "\n"
     << "A - Add a number" << "\n"
     << "M - Display mean of the numbers" << "\n"
     << "S - Display the smallest number" << "\n"
     << "L - Display the largest number" << "\n"
-    << "Q - Quit" << "\n";
+    << "C - Count the occurences of a number" << "\n"
+    << "E - Empty the list" << "\n"
+    << "Q - Quit" << "\n\n";
     cin >> c;
     switch (c)
     {
@@ -111,7 +113,7 @@ void main(){
                 cout << "Unable to calculate the mean - no data";
             else {
                 double mean{0};
-                int total{0};
+                double total{0};
                 for (auto _n: nums) 
                 {
                     total += _n;
@@ -147,16 +149,58 @@ void main(){
         break;
     case 'L':
     case 'l':
-        /* code */
+        /* If the user enters 'L' or 'l' you should display the largest element in the list
+           For example, if the list contains [2 4 5 1], you should display, "The largest number is 5"
+           If the list is empty you should display, "Unable to determine the largest number - list is empty"
+        */
+        {
+            if (nums.empty()) 
+                cout << "Unable to determine the largest number - list is empty";
+            else {
+                int largest{0};
+                for (int idx=0; idx < nums.size(); idx++){
+                    if (idx==0)
+                        largest = nums[idx];
+                    else if (nums[idx] > largest)
+                        {
+                            largest = nums[idx];
+                        };
+                    };
+                cout << "The largest number is " << largest;
+                }
+        }       
         break;
+    case 'C':
+    case 'c':
+        /*- search for a number in the list and if found display the number of times it occurs in the list*/
+        {
+            int check{0};
+            int occurences{0};
+            cout << "Input number to count occurences of that number: ";
+            cin >> check;
+            for (int n: nums){
+                if (n == check)
+                    occurences +=1;
+        }
+        cout << check << " occured " << occurences << " times!";
+        }
+        break;
+    case 'E':
+    case 'e':
+        nums.clear();
+        cout << "All saved numbers have been cleared";
+        break;
+ 
+    
     case 'Q':
     case 'q':
-        break;   
+        break;
     default:
         cout << "Invalid selection - " << c;
         break;
     }
     } while (
         (c != 'q') & (c != 'Q')
-    );  
+    );
+    cout << "Goodbye!";  
 }
