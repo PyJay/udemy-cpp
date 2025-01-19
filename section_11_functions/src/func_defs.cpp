9f16469f71d69000dfc55da5df8403b3c100e09f
Jay Shah <jrshah01@gmail.com>
2025-01-19 20:38:16 +0000

    before refactor

--- section_11_functions/src/func_defs.cpp
+++ section_11_functions/src/func_defs.cpp
@@ -12,14 +12,28 @@ void area_circle(){
     double radius{};
     cout << "\nEnter the radius of the circle: ";
     cin >> radius;
-
     cout << "The area of a circle with radius " << radius << " is " << calc_area_circle(radius) << endl;
+}
 
+
+double calc_volume_cylinder(double radius, double height) {
+    return pi * radius * radius * height;
+}
+
+
+void volume_cylinder(){
+    double radius{};
+    double height{};
+    cout << "\nEnter the radius of the cylinder: ";
+    cin >> radius;
+    cout << "\nEnter the height of the cylinder: ";
+    cin >> height;
+    cout << "The volume of a cylinder with radius " << radius << " and height " << height << " is " << calc_volume_cylinder(radius, height) << endl;
 }
 
 
 int main(){
     area_circle();
     volume_cylinder();
-    return 0
+    return 0;
 }
