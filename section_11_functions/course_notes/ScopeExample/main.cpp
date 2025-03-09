1fa3ff2d915b1f390fe4b40a4b679296716729a4
Jay Shah <jrshah01@gmail.com>
2025-03-09 18:48:02 +0000

    making notes thru the scope example code

--- section_11_functions/course_notes/ScopeExample/main.cpp
+++ section_11_functions/course_notes/ScopeExample/main.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+// function prototypes
 void local_example();
 void global_example();
 void static_local_example();
@@ -11,24 +12,24 @@ void static_local_example();
 int num {300};    // Global variable - declared outside any class or function
 
 void global_example() {
-    cout << "\nGlobal num is: " << num << " in global_example - start" << endl;
+    cout << "\nGlobal num is: " << num << " in global_example - start" << endl; //300
     num *= 2;
-    cout << "Global num is: " << num << " in global_example - end" << endl;
+    cout << "Global num is: " << num << " in global_example - end" << endl; //600
 }
 
 void local_example(int x) {
     int num {1000};     // local to local_example
-    cout << "\nLocal num is: " << num << " in local_example - start" << endl;
+    cout << "\nLocal num is: " << num << " in local_example - start" << endl; // 1000
     num=x;
-    cout << "Local num is: " << num << " in local_example - end" << endl;
+    cout << "Local num is: " << num << " in local_example - end" << endl; // first 10 and then 20
     // num1 in main is not within scope - so it can't be used here.
 }
 
 void static_local_example() {
     static int num {5000};      // local to static_local_example static - retains it value between calls
-    cout << "\nLocal static  num is: " << num << " in static_local_example - start" << endl;
-    num += 1000;
-    cout << "Local static  num is: " << num << " in static_local_example - end" << endl;
+    cout << "\nLocal static  num is: " << num << " in static_local_example - start" << endl; // 5000
+    num += 1000; // 6000
+    cout << "Local static  num is: " << num << " in static_local_example - end" << endl; // 6000
 }
 
 int main() {
@@ -36,15 +37,15 @@ int main() {
     int num {100};  // Local to main
     int num1 {500}; // Local to main
     
-    cout << "Local num is : " << num << " in main" << endl;
+    cout << "Local num is : " << num << " in main" << endl; //100
     
     {   // creates a new level of scope
         int num {200};  // local to this inner block
-        cout << "Local num is: " << num << " in inner block in main" << endl;
-        cout << "Inner block in main can see out - num1 is: " << num1 << endl;
+        cout << "Local num is: " << num << " in inner block in main" << endl; //200
+        cout << "Inner block in main can see out - num1 is: " << num1 << endl; //500
     }
     
-    cout << "Local num is : " << num << " in main" << endl;
+    cout << "Local num is : " << num << " in main" << endl; //100
 
     local_example(10);
     local_example(20);
@@ -53,8 +54,8 @@ int main() {
     global_example();
     
     static_local_example();
-//    static_local_example();
-//    static_local_example();
+    static_local_example(); // the num will increment (1_000)
+    static_local_example(); // and again
 
     cout << endl;
     return 0;
